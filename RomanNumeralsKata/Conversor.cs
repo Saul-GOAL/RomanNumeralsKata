b13bff05c65a8bd502d67c79f795eafc89d38918
Sauru <saul_gonzalez_almenara@hotmail.com>
2022-07-06 10:08:15 +0200

    Project Preparation + First test

---
+++ RomanNumeralsKata/Conversor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals
+{
+    public static class Conversor
+    {
+        public static string Convert(int testElement)
+        {
+            return "I";
+        }
+    }
+}
--- RomanNumeralsTest/UnitTest1.cs
+++ RomanNumeralsTest/UnitTest1.cs
@@ -1,19 +1,24 @@
-namespace RomanNumeralsTest
+using RomanNumerals;
+
+namespace RomanNumeralsTest 
 {
     public class Tests
     {
-
+        //Write a method "string convert(int)" that takes a number and converts it to the according String representation.
 
         [Test]
-        public void Test1()
+        public void Given_The_Integer_1_It_Will_Return_The_String_I()
         {
             //Arrange
-
+            int testElement = 1;
+            string expectedResult = "I";
 
             //Act
-
+            string romanNumeral = Conversor.Convert(testElement);
 
             //Assert
+            Assert.That(romanNumeral, Is.EqualTo(expectedResult));
+
         }
     }
 }
