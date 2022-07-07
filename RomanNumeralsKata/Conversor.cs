ef7dcd7342f87345f6b97f3fbf2ec5f78ae9b889
Sauru <saul_gonzalez_almenara@hotmail.com>
2022-07-07 11:37:12 +0200

    minor refactoring to remove hand-placed values adding Constants

--- RomanNumeralsKata/Conversor.cs
+++ RomanNumeralsKata/Conversor.cs
@@ -8,6 +8,20 @@ namespace RomanNumerals
 {
     public static class Conversor
     {
+        private const string Roman_1000 = "M";
+        private const string Roman_900 = "CM";
+        private const string Roman_500 = "D";
+        private const string Roman_400 = "CD";
+        private const string Roman_100 = "C";
+        private const string Roman_90 = "XC";
+        private const string Roman_50 = "L";
+        private const string Roma_40 = "XL";
+        private const string Roman_10 = "X";
+        private const string Roman_9 = "IX";
+        private const string Roman_5 = "V";
+        private const string Roman_4 = "IV";
+        private const string Roman_1 = "I";
+
         public static string Convert(int number)
         {
             var romanNumber = "";
@@ -15,77 +29,83 @@ namespace RomanNumerals
             if (number >= 1000)
             {
                 int result = number / 1000;
-                romanNumber = Concatenation(result, romanNumber, "M"); ;
+                romanNumber = Concatenation(result, romanNumber, Roman_1000); ;
                 number -= 1000 * result;
             }
 
             if (number >= 900)
             {
-                romanNumber += "CM";
+                romanNumber += Roman_900;
                 number -= 900;
             }
 
             if (number >= 500)
             {
-                romanNumber += "D";
+                romanNumber += Roman_500;
                 number -= 500;
             }
 
             if (number >= 400)
             {
-                romanNumber += "CD";
+                romanNumber += Roman_400;
                 number -= 400;
             }
 
             if (number >= 100)
             {
                 int result = number / 100;
-                romanNumber = Concatenation(result, romanNumber, "C"); ;
+                romanNumber = Concatenation(result, romanNumber, Roman_100); ;
                 number -= 100 * result;
             }
 
             if (number >= 90)
             {
-                romanNumber += "XC";
+                romanNumber += Roman_90;
                 number -= 90;
             }
 
             if (number >= 50)
             {
-                romanNumber += "L";
+                romanNumber += Roman_50;
                 number -= 50;
             }
             
             if (number >=40)
             {
-                romanNumber += "XL";
+                romanNumber += Roma_40;
                 number -= 40;
             }
             if (number >= 10)
             {
                 int result = number / 10;
-                romanNumber = Concatenation(result, romanNumber, "X"); ;
+                romanNumber = Concatenation(result, romanNumber, Roman_10); ;
                 number -= 10*result;
             }
 
             if (number == 9)
-                return romanNumber + "IX";
+            {
+                romanNumber += Roman_9;
+                number -= 9;
+            }
             
             if (number >= 5)
             {
-                romanNumber += "V";
+                romanNumber += Roman_5;
                 number -= 5;
             }
 
             if (number == 4)
-                return romanNumber + "IV";
+            {
+                romanNumber += Roman_4;
+                number -= 4;
+            }
 
         return Concatenation(number, romanNumber);
 
         }
 
 
-        private static string Concatenation(int number, string romanNumber,string iterable = "I")
+        private static string Concatenation(int number, string romanNumber,string iterable = Roman_1)
         {
             for (var i = 0; i < number; i++)
             {
